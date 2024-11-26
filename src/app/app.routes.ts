9b5868d25feaa2a63c92187c1240b4cc5979136f
Ahmansavthor Batista <ahmansavthor.batista@ithealth.co>
2024-11-26 12:09:11 -0500

    Feat: Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: '',
-        loadComponent: () => import('./login/login.component').then(m => m.LoginComponent)
-    }
+        loadChildren: () => import('./login/login.routes').then(m => m.ROUTES_LOGIN)
+    }   
 ];
