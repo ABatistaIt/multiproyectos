592c7a0112f5ab5ac2e2e83ea6f3199107c35a73
Ahmansavthor Batista <ahmansavthor.batista@ithealth.co>
2024-11-26 12:11:58 -0500

    Fix: routeing

--- src/app/login/login.routes.ts
+++ src/app/login/login.routes.ts
@@ -4,7 +4,12 @@ export const ROUTES_LOGIN: Routes = [
     {
         path: '',
         loadComponent: () => import('./login.component').then(m => m.LoginComponent),
-        children: []
+        children: [
+            {
+                path: 'livelis',
+                loadComponent: () => import('../projects/livelis/livelis-layout.component').then(m => m.LivelisLayoutComponent)
+            }
+        ]
     },
     
 ]
